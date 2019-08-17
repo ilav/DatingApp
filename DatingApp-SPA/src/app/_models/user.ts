f662b8192b37a52954599f9d2ad25274f1587079
Igor Babenko <igorb@cosmostv.by>
2019-08-17 18:08:27 +0300

    end of section 9

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,23 @@
+import { Photo } from './photo';
+
+export interface User {
+  id: number;
+  userName: string;
+  knownAs: string;
+  age: number;
+  gender: string;
+  created: Date;
+  lastActive: Date;
+  photoUrl: string;
+  city: string;
+  country: string;
+
+  // This is optional that is why we use the 'elvies' or '?' operator
+  // Optional properties should come after the required properties or else you will get
+  // an error
+  interest?: string;
+  introduction?: string;
+  lookingFor?: string;
+  photos?: Photo[];
+  //roles?: string[];
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MemberCardComponent } from './members/member-card/member-card.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { AlertifyService } from './_services/alertify.service';
 import { ErrorInterceptorProvider } from './_services/error.interseptor';
@@ -5,19 +6,28 @@ import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { JwtModule } from '@auth0/angular-jwt';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { appRoutes } from './routes';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { BsDropdownModule } from 'ngx-bootstrap';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
+import { UserService } from './_services/user.service';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,20 +36,37 @@ import { appRoutes } from './routes';
     RegisterComponent,
     MemberListComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    NgxGalleryModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    TabsModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        // In production, if we will have our Angular pages hosted in the same
+        // domain with the API, for example under the API wwwroot folder "../DatingApp.API/wwwroot",
+        // then we don't need to specify a whitelistedDomains
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
   providers: [
     AuthService,
     ErrorInterceptorProvider,
     AlertifyService,
-    AuthGuard
+    AuthGuard,
+    UserService,
+    MemberDetailResolver,
+    MemberListResolver
   ],
   bootstrap: [AppComponent]
 })
